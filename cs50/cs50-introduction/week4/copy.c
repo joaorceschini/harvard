3632b5bb976315dde3bbf6e0dfd03e4ea3809018
joaorceschini <jrvceschini@gmail.com>
2024-09-10 21:33:48 -0300

    week4 copy

---
+++ cs50/cs50-introduction/week4/copy.c
@@ -0,0 +1,17 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+  char *s = get_string("s: ");
+
+  char *t = s;
+
+  if (strlen(t) > 0) {
+    t[0] = toupper(t[0]);
+  }
+
+  printf("%s\n", s);
+  printf("%s\n", t);
+}
