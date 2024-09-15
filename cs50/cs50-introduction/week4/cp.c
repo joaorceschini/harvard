9d2621a07d3ca3c6ecf61e90fa4f75018a08e208
joaorceschini <jrvceschini@gmail.com>
2024-09-15 20:31:09 -0300

    week4 cp

---
+++ cs50/cs50-introduction/week4/cp.c
@@ -0,0 +1,16 @@
+#include <stdint.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  FILE *src = fopen(argv[1], "rb");
+  FILE *dst = fopen(argv[2], "wb");
+
+  uint8_t b;
+
+  while (fread(&b, sizeof(b), 1, src) != 0) {
+    fwrite(&b, sizeof(b), 1, dst);
+  }
+
+  fclose(src);
+  fclose(dst);
+}
